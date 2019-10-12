9d5c75aa6be1890cfabc17508f25fc7d7d880d8f
Pablo Andres <pabloando99@gmail.com>
2019-10-12 12:24:37 +0200

    Removed password validation

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import AuthContext from '../../context/auth/authContext';
-import Users from '../users/users';
-import UserFilter from '../users/UserFilter';
+import Users from '../user/Users';
+import UserFilter from '../user/UserFilter';
 
 const Home = () => {
   const authContext = useContext(AuthContext);
